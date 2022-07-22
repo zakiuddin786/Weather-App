16f2130ca814cf7b790c3c8866e3aaaa0d53b467
Mohammed Zaki <zakiuddin321@gmail.com>
2022-07-22 20:02:47 +0530

    added 2 separate functions for getting the current weather and the forcast

--- backend/lib/weather.js
+++ backend/lib/weather.js
@@ -2,9 +2,20 @@ const axios = require("axios")
 const config = require("../config/config");
 const env_config = config.get_active_config();
 
-exports.getWeather = async (req,res)=>{
+exports.getWeatherForecast = async (req,res)=>{
     let api_key = env_config.weatherAPI;
-    let url = env_config.API_URL + "lat=35&lon=139" + "&appid=" + api_key;
+    let url = `${env_config.API_URL}/forecast?q=hyderabad&appid=${api_key}`;
+    
+    console.log(url);
+    let data = await axios.get(url);
+    let weather_info = data.data;
+    return res.status(200).send(weather_info)
+}
+
+exports.getCurrentWeather = async (req,res)=>{
+
+    let api_key = env_config.weatherAPI;
+    let url = `${env_config.API_URL}/weather?q=hyder&appid=${api_key}`;
 
     let data = await axios.get(url);
     let weather_info = data.data;
